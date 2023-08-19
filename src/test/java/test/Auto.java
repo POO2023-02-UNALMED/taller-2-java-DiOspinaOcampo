370c8244956a0768672ddc5489da36b9012b69ab
DiOspinaOcampo <jospinaoc@unal.edu.co>
2023-08-19 10:04:20 -0500

    Commit3

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,48 +1,47 @@
-
 package test;
-public class Auto {
-	    String modelo;
-	    int precio;
-	    Asiento[] asientos;
-	    String marca;
-	    Motor motor;
-	    int registro;
-	    int cantidadCreados;
-	    
-	    public Auto(String modelo, int precio, Asiento[] asientos, String marca, Motor motor, int registro) {
-	        this.modelo = modelo;
-	        this.precio = precio;
-	        this.asientos = asientos;
-	        this.marca = marca;
-	        this.motor = motor;
-	        this.registro = registro;
-	        Auto.cantidadCreados++;
-	    }
-	    
-	    public int cantidadAsientos(){
-	        int numeroAsientos = 0;
-	        for(Asiento asiento: this.asientos) {
-	            if(asiento != null) {
-	                numeroAsientos++;
-	            }
-	        }
-	        
-	        return numeroAsientos;
-	    }
-	    
-	    public String verificarIntegridad() {
-	        String mensajeErroneo = "Las piezas no son originales";
-	        if(this.registro != this.motor.registro) {
-	            return mensajeErroneo;
-	        }
-	        
-	        for(Asiento asiento: this.asientos) {
-	            if(this.registro != asiento.registro) {
-	                return mensajeErroneo;
-	            }
-	        }
-	        
-	        return "Auto original";
-	    }
 
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados = 0;
+    
+    public Auto(String modelo, int precio, Asiento[] asientos, String marca, Motor motor, int registro) {
+        this.modelo = modelo;
+        this.precio = precio;
+        this.asientos = asientos;
+        this.marca = marca;
+        this.motor = motor;
+        this.registro = registro;
+        Auto.cantidadCreados++;
+    }
+    
+    public int cantidadAsientos(){
+        int numeroAsientos = 0;
+        for(Asiento asiento: this.asientos) {
+            if(asiento != null) {
+                numeroAsientos++;
+            }
+        }
+        
+        return numeroAsientos;
+    }
+    
+    public String verificarIntegridad() {
+        String mensajeErroneo = "Las piezas no son originales";
+        if(this.registro != this.motor.registro) {
+            return mensajeErroneo;
+        }
+        
+        for(Asiento asiento: this.asientos) {
+            if(this.registro != asiento.registro) {
+                return mensajeErroneo;
+            }
+        }
+        
+        return "Auto original";
+    }
 }
