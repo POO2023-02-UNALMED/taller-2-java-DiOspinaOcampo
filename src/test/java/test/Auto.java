4f2df9760d1ae8a38f7744d80372f29f66ae0508
DiOspinaOcampo <jospinaoc@unal.edu.co>
2023-08-19 10:30:12 -0500

    Commit
    será el final?

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,4 @@
 package test;
-
 public class Auto {
     String modelo;
     int precio;
@@ -41,7 +40,7 @@ public class Auto {
         }
         
         for(Asiento asiento: this.asientos) {
-            if(this.registro != asiento.registro) {
+            if(asiento != null && this.registro != asiento.registro) {
                 return mensajeErroneo;
             }
         }
